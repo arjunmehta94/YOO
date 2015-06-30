4616145d372fbebf5cd16433c1bc7d8c6d741dfa
Arjun Mehta <arjunmehta94@berkeley.edu>
2015-06-30 21:34:11 +0530

    updating arjun branch

--- CodeBluetooth/libraries/BluetoothHandler/BluetoothHandler.cpp
+++ CodeBluetooth/libraries/BluetoothHandler/BluetoothHandler.cpp
@@ -6,116 +6,111 @@ static const char pause = '$';
 static const char stop = '!';
 HardwareSerial* printer;
  
-
 //<<constructor>> begin serial comm
-BluetoothHandler::BluetoothHandler(HardwareSerial &print)
-{	
-	queue = new QueueList<String>();
+BluetoothHandler::BluetoothHandler(HardwareSerial &print) {
 	printer = &print;
 	printer->begin(9600);
 }
- 
+
 //<<destructor>>
-BluetoothHandler::~BluetoothHandler()
-{
+BluetoothHandler::~BluetoothHandler() {
 	/*nothing to destruct*/
 }
 
 //Send databyte 
-String BluetoothHandler::processNumber(signed int num)
-{
-	signed int tmp = abs(num); 
+
+void BluetoothHandler::checkLibrary() {
+	printer->print("this is the library!!!!");
+}
+
+void BluetoothHandler::send(int8_t numberX, int8_t numberY, char mode) {
+	int8_t tmpX = abs(numberX);
+	int8_t tmpY = abs(numberY);
 	String ret;
-	
-	if(tmp < 10) 
-	{
-		if(num > 0) // if num between 0 and 9
-		{
-			ret = "+" + "00" + tmp.toString();
+
+	if(tmpX < 10) {
+		if(numberX > 0) {// if numX between 0 and 9
+			ret = "+";
+			ret += "00";
+			ret += String(tmpX);
 		}
-		else // if num between 0 and -9
-		{
-			ret = "-" + "00" + tmp.toString();
+		else {// if num between 0 and -9
+			ret = "-";
+			ret += "00";
+			ret += String(tmpX);
 		}
-		
 	}
-	else if(tmp < 100)
-	{
-		if (num > 0) // if num between 10 and 99
-		{
-			ret = "+" + "0" + tmp.toString();
+	else if(tmpX < 100) {
+		if (numberX > 0) {// if numX between 10 and 99
+			ret = "+"; 
+			ret += "0";
+			ret += String(tmpX);
 		}
-		else // if num between -10 and -99
-		{
-			ret = "-" + "0" + tmp.toString();
+		else {// if numX between -10 and -99
+			ret += "-";
+			ret += "0"; 
+			ret += String(tmpX);
 		}
-		
 	}
-	else if(tmp > 100)
-	{
-		if (num > 0) // if num between 100 and 127
-		{
-			ret = "+" + tmp.toString();
+	else if(tmpX > 100) {
+		if (numberX > 0) {// if numX between 100 and 127
+			ret = "+";
+			ret += String(tmpX);
 		}
-		else // if num between -100 and -128
-		{
-			ret = "-" + tmp.toString();
+		else {// if numX between -100 and -128
+			ret = "-";
+			ret += String(tmpX);
 		}
-		
 	}
-	
-	return ret;	
+
+	if(tmpY < 10) {
+		if(numberY > 0) {// if numY between 0 and 9
+			ret += "+"; 
+			ret += "00"; 
+			ret += String(tmpY);
+		}
+		else {// if numY between 0 and -9
+			ret += "-";
+			ret += "00";
+			ret += String(tmpY);
+		}
+	}
+	else if(tmpY < 100) {
+		if (numberY > 0) {// if num between 10 and 99
+			ret += "+";
+			ret += "0";
+			ret += String(tmpY);
+		}
+		else {// if numY between -10 and -99
+			ret += "-";
+			ret += "0";
+			ret += String(tmpY);
+		}
+	}
+	else if(tmpY > 100) {
+		if (numberY > 0) {// if numY between 100 and 127
+			ret += "+";
+			ret += String(tmpY);
+		}
+		else {// if numY between -100 and -128
+			ret += "-";
+			ret += String(tmpY);
+		}
+	}
+	ret += mode;
+	ret += ")";
+	printer->print(ret);
 }
 
-// format to send: "x, y, play/pause, )" -> "!!!!????*)"
-void BluetoothHandler::send(signed int x, signed int y) 
-{
-		String strX, strY, ret;
-		strX = BluetoothHandler::processNumber(x);
-		strY = BluetoothHandler::processNumber(y);
-		ret = strX + strY + play + ")";
-        printer->print(ret);
-}
-
-//penDown
-
-void BluetoothHandler::penDown()
-{
+void BluetoothHandler::penDown() {
 	printer->print(play);
 }
  
 // temporary pen lift
-void BluetoothHandler::penUp()
-{
+void BluetoothHandler::penUp() {
         printer->print(pause);
 }
  
-void BluetoothHandler::endWrite()
-{
+void BluetoothHandler::endWrite() {
 	printer->print(stop);
-}
-
-void BluetoothHandler::pushBuffer(String item)
-{
-	queue.push(item);
-}
-
-String BluetoothHandler::popBuffer()
-{
-	queue.pop();
-}
-
-String BluetoothHandler::peekBuffer()
-{
-	queue.peek();
-}
-
-bool BluetoothHandler::isEmptyBuffer()
-{
-	queue.isEmpty();
-}
-
-int BluetoothHandler::countBuffer()
-{
-	queue.count();
 }
