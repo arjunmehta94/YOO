9f78f20f155b725fb8912d23c9fc346a7058db30
Anurag Sahoo <anurag@Anurags-MacBook-Pro.local>
2015-08-05 15:17:48 +0530

    added website files

---
+++ index.js
@@ -0,0 +1,12 @@
+/*** @jsx React.DOM */
+var Alert = ReactBootstrap.Alert;
+var NavBar = React.createClass({
+	render : function() {
+		return (
+			<h1>adsf</h1>
+			//console.log(Alert)
+		);
+	}
+});
+
+React.render(<NavBar/>, document.getElementById('mount-point'));
